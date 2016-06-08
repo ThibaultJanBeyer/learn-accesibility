b3b94cf1fdaac76ac22f03020acfe262f3865018
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2016-06-08 15:47:29 +0200

    accessible menu, accessibility enhancements, fix layout

--- src/assets/javascript/home.js
+++ src/assets/javascript/home.js
@@ -1,5 +1,11 @@
 (function () { if( R_HOME ){
 
   BODY.classList.add('lock');
+  setTimeout(function() {
+    var start = document.getElementsByClassName('start')[0];
+    var end = document.getElementsByClassName('end')[0];
+    start.classList.add('sr-only');
+    end.classList.remove('sr-only');
+  }, 10000);
 
 }})();
