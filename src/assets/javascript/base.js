dcea8119b97ee81c35cd613e3291668f75e48d76
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2016-06-10 15:55:09 +0200

    add & style main nav

--- src/assets/javascript/base.js
+++ src/assets/javascript/base.js
@@ -2,10 +2,13 @@
 const BODY = document.getElementsByTagName('body')[0];
 const WELCOME = document.getElementById('welcome');
 var MOUSE = {};
+var LOCATION = window.location.href;
 
 // Routes
-if ( window.location.href.indexOf('home') >= 0 ) {
+if ( LOCATION.indexOf('home') >= 0 ) {
   const R_HOME = true;
+} else if ( LOCATION.indexOf('visual') >= 0 ) {
+  const R_SITE = true;
 } else {
   const R_WELCOME = true;
 }
---
+++ src/assets/javascript/site.js
@@ -0,0 +1,29 @@
+(function () { if( R_SITE ){
+
+  /*
+   * Navigation
+   */
+  var site = document.getElementsByClassName('site')[0];
+  var siteToggler = document.getElementsByClassName('site__toggler')[0];
+
+  siteToggler.addEventListener('click', () => {
+    site.classList.toggle('site--open');
+    siteToggler.classList.toggle('site__toggler--open');
+  });
+
+
+  var navLink = document.getElementsByClassName('nav__link');
+
+  for (let i = 0; i < navLink.length; i++) {
+    navLink[i].addEventListener('click', () => {
+      let clicked = this;
+      for (let i = 0; i < navLink.length; i++) {
+        navLink[i].classList.remove('nav__link--selected')
+      }
+      clicked.classList.add('nav__link--selected');
+    });
+  }
+
+
+
+}})();
