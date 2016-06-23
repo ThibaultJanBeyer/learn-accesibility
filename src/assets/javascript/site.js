8bc82cba69e040cc4cd59f9b507dee50f938c0ca
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2016-06-23 18:53:25 +0200

    add visual hint of current section, fix overflow bug on mobile, Add margin bottom to tables, fix TOC z-index

--- src/assets/javascript/site.js
+++ src/assets/javascript/site.js
@@ -64,4 +64,48 @@
     });
   }
 
+  /*
+   * Check which sections the user is on to update the TOC accordingly
+   */
+  setTimeout(function() { // a small delay so that everything else is successfully rendered
+    // store the position of each section - 1/4 of the window height in an array
+    let sectionsPos = [];
+    [...document.getElementsByClassName('section__title')].forEach((e, i) => {
+      sectionsPos.push(e.offsetTop - window.innerHeight / 4);
+    });
+    
+    // store all Links in a variable.
+    let tocLinks = document.getElementsByClassName('toc__link');
+    // set a boolean that will see if we are scrolling
+    let scrolling = false;
+    // set that bool = true if we do
+    window.onscroll = () => { scrolling = true; };
+
+    // every 2,5 seconds check if a scroll has happened
+    setInterval(function() {
+      if (scrolling) {
+        // is so, then store the scroll position
+        let scroll = document.documentElement.scrollTop || document.body.scrollTop;
+        // and compare it to each sections position
+        for (let i = 0; i < sectionsPos.length; i++) {
+          // store the corresponding link. How convenient, that we always have the same amout of links as sections
+          let tocLink = tocLinks[i];
+          // if it is within the range from the current one to the next one
+          if (scroll > sectionsPos[i] && scroll < sectionsPos[i + 1]) {
+            // give the link the current class 
+            tocLink.classList.add('toc__link--current');
+            tocLink.setAttribute('aria-label', 'current');
+            window.hash = tocLink.href;
+          } else {
+            // if it is not in gange, then remove the class
+            tocLink.classList.remove('toc__link--current');
+            tocLink.removeAttribute('aria-label');
+          }
+        }
+      }
+      // and set the scrolling to false again until next scroll
+      scrolling = false;
+    }, 2500);
+  }, 1000);
+
 }})();
